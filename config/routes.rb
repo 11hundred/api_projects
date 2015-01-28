b4e074154ad75977b1fd9f3e660a4e62aa39d8ca
David Handschuh <david@11hundred.com>
2015-01-28 12:49:30 -0600

    David's rails API project implemented the hash param

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   
   get("/", { :controller => "pages", :action => "home" })
-  get("/weather/", { :controller => "forecasts", :action => "location_search" })
+  get("/weather/", { :controller => "forecasts", :action => "location" })
   get("/weather/:address", { :controller => "forecasts", :action => "location" })
   
   
